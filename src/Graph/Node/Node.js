0cad7bccf57146aa9e8d245e9fbfdf75cccb3a87
Munindra Naik <naikmunindra@gmail.com>
2020-08-08 16:14:54 +0530

    formed the grid

--- src/Graph/Node/Node.js
+++ src/Graph/Node/Node.js
@@ -14,15 +14,16 @@ export default class Node extends Component {
             pointer_Now,
         }=this.props;
 
-        const extraClassName = dest ?'node-dest':src?'node-src':wall?'node-wall':'';
+        const css_style = dest ?'node-dest':src?'node-src':wall?'node-wall':'';
 
     return (
       <div
         id={`node-${row}-${col}`}
-        className={`node ${extraClassName}`}
+        className={`node ${css_style}`}
         pointer_Down={() => pointer_Down(row, col)}
         pointer_Now={() => pointer_Now(row, col)}
-        pointer_Up={() => pointer_Up()}></div>
+        pointer_Up={() => pointer_Up()}>
+        </div>
     );
     }
 }
--- src/Graph/Visualizer/Visualizer.js
+++ src/Graph/Visualizer/Visualizer.js
@@ -2,8 +2,38 @@ import React , {Component} from 'react';
 import Node from '../Node/Node.js';
 import { render } from '@testing-library/react';
 
+
 export default class Visualizer extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            nodes: [],
+        }
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for(let row =0; row < 25 ; row++) {
+            const curRow = [];
+            for(let col=0; col<50; col++){
+                curRow.push([]);
+            }
+            nodes.push(curRow);
+        }
+        this.setState({nodes});
+    }
+
     render(){
-        return(<Node></Node>);
+        const {nodes} = this.state;
+
+        return(
+        <div>
+        {nodes.map((row,rowIdx) => {
+            return <div>
+                {row.map((node,nodeIdx) => <Node></Node>)}
+            </div>
+        })}
+        </div>
+        );
     }
 }
